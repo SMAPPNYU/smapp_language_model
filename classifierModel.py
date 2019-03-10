c2faf2ac5c22da47d0c008d176316ff346813154
vishakhpk <vishakhpadmakumar@gmail.com>
2019-03-10 22:19:00 +0000

    Found bug related to hidden layer dimensions in Encoder Model

--- classifierModel.py
+++ classifierModel.py
@@ -13,14 +13,19 @@ from data_utils import (IndexVectorizer,
                         TextDataset,
                         LMDataLoader,
                         CLFDataLoader)
+from model import _detach
 
 class EncoderModel(torch.nn.Module):
-    def  __init__(self, vectorizer, hidden_size, embedding_size, bidirectional = False, num_layers=3, tie_weights=False):
+    def  __init__(self, device, vectorizer, hidden_size, embedding_size, bidirectional = False, batch_size = 50, num_layers=3, tie_weights=False):
         super(EncoderModel, self).__init__()
         self.dropout = nn.Dropout(p = 0.5)
         self.num_directions = 1 if not bidirectional else 2
         self.tie_weights = tie_weights
         self.num_layers = num_layers
+        self.batch_size = batch_size
+        self.device = device
+        self.hidden_size = hidden_size
+        self.embedding_size = embedding_size
         self.encoder = nn.Embedding(vectorizer.vocabulary_size, embedding_size, padding_idx = 0)
         self.rnns = [nn.LSTM(embedding_size if l == 0 else hidden_size * self.num_directions,
                               embedding_size if (l == self.num_layers-1 and self.tie_weights) else hidden_size,
@@ -28,14 +33,39 @@ class EncoderModel(torch.nn.Module):
                              bidirectional = bidirectional,
                              batch_first = True) for l in range(num_layers)]
         self.rnns = nn.ModuleList(self.rnns)
-        
+        self.hidden = [self._reset_hidden_layer(layer_index=l) for l in range(num_layers)]
+
+    def _reset_hidden_layer(self, layer_index, bsz=None):
+        '''
+        Resets (or initalizes) the initial hidden (h0) and output (c0) for an LSTM.
+        Returns a tuple of tensors!
+        '''
+        if bsz == None: 
+            bsz = self.batch_size
+        if layer_index == self.num_layers - 1 and self.tie_weights:
+            dim = self.embedding_size
+        else:
+            dim = self.hidden_size
+        h0 = torch.zeros(self.num_directions, bsz, 
+                         dim).to(self.device)
+        c0 = torch.zeros(self.num_directions, bsz, 
+                         dim).to(self.device)
+        return (h0, c0)
+
     def forward(self, input_):
 
-        x = self.encoder(input_)
+        output = self.encoder(input_)
         #print("Input shape after embeddings: ", x.shape)
-        hidden = None
+        new_hidden = []
         for l, rnn in enumerate(self.rnns):
-            output, hidden = rnn(x if l==0 else self.dropout(output), None if l==0 else hidden)
+            #print(l, x.shape)
+            #if hidden is not None:
+            #    print(hidden.shape)
+            output, hidden = rnn(self.dropout(output) if l!= self.num_layers-1 else output, self.hidden[l])
+            #print(output.shape, hidden[0].shape)
+            new_hidden.append(hidden)
+        self.hidden = [_detach(h, cpu=False) for h in new_hidden]
+        #self.hidden = [h for h in new_hidden]
         return output # just return the hidden states
 
 class ClassifierModel(torch.nn.Module):
