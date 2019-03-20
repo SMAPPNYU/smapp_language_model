f5dd54e1930e0014b71fd29fdad44a625062c7ab
vishakhpk <vishakhpadmakumar@gmail.com>
2019-03-20 17:36:26 +0000

    Added gradual unfreezing for LM

--- software.py
+++ software.py
@@ -17,9 +17,8 @@ import pickle
 
 class SMaPPLearn:
     
-    def __init__(self, data_dir, train_file, valid_file, max_vocab_size = 20000, batch_size = 50, 
-            revectorize = False):
-        
+    def __init__(self, data_dir, train_file, valid_file, 
+            max_vocab_size = 20000, batch_size = 50, revectorize = False):
         self.data_dir = data_dir
         self.TOKENIZE = SpacyTokenizer().tokenize
         self.MIN_WORD_FREQ = 2
@@ -35,13 +34,15 @@ class SMaPPLearn:
         # GPU setup
         self.use_gpu = torch.cuda.is_available()
         device_num = 0
-        self.device = torch.device(f"cuda:{device_num}" if self.use_gpu else "cpu")
+        self.device = torch.device(f"cuda:{device_num}" if self.use_gpu 
+            else "cpu")
 
         # IO setup
         today = datetime.datetime.now().strftime('%Y-%m-%d')
         model_cache_dir = os.path.join(data_dir, 'models')
         self.data_cache = os.path.join(model_cache_dir, 'data_cache.pkl')
-        self.vectorizer_cache = os.path.join(model_cache_dir, 'lm_vectorizer.pkl')
+        self.vectorizer_cache = os.path.join(model_cache_dir, 
+            'lm_vectorizer.pkl')
         os.makedirs(model_cache_dir, exist_ok=True)
         self.model_file_lm = os.path.join(model_cache_dir, f'LM__{today}.json')
         
@@ -63,14 +64,17 @@ class SMaPPLearn:
             self.valid_ds = TextDataset(data=valid, vectorizer=self.vectorizer, 
                 text_col='text')
             
-            pickle.dump([self.train_ds, self.valid_ds], open(self.data_cache, 'wb'))
+            pickle.dump([self.train_ds, self.valid_ds], open(self.data_cache, 
+                'wb'))
             pickle.dump(self.vectorizer, open(self.vectorizer_cache, 'wb'))
         else:
-            self.train_ds, self.valid_ds = pickle.load(open(self.data_cache, 'rb'))
+            self.train_ds, self.valid_ds = pickle.load(open(self.data_cache, 
+                'rb'))
             self.vectorizer = pickle.load(open(self.vectorizer_cache, 'rb'))
         
         print("Vectorizing is complete.")        
-        print(f'Train size: {len(self.train_ds)}\nvalid size: {len(self.valid_ds)}')
+        print(f'Train size: {len(self.train_ds)}\n \
+            valid size:{len(self.valid_ds)}')
         print(f"Vocab size: {len(self.vectorizer.vocabulary)}")
         
         self.train_dl = LMDataLoader(dataset=self.train_ds, 
@@ -90,7 +94,21 @@ class SMaPPLearn:
                                 batch_size=self.batch_size) 
         
         print("Created Data Loaders for documents")
-        
+    
+    def freezeTo(self, n):
+        if n>1 and n<=1+self.lstm_layers:
+            n = 1 + (n-1)*4
+        print("Freezing layers until ", n)
+        i=0
+        for name, p in self.lm.named_parameters():
+            if i<n or n==-1:
+                p.requires_grad = False
+            else:
+                p.requires_grad = True
+            i+=1
+        for name, p in self.lm.named_parameters():
+            print(name, p.requires_grad)
+    
     def fit_language_model(self, pretrained_itos = None,
             pretrained_weight_file = None, lm_hidden_dim = 1150, 
             lm_embedding_dim = 400, lm_lstm_layers = 3, num_epochs=100, 
@@ -114,9 +132,11 @@ class SMaPPLearn:
         
         if pretrained_weight_file is not None and pretrained_itos is not None: 
             print("Starting Loading pretrained Wikitext model")
-            enc = torch.load(pretrained_weight_file, map_location=lambda storage, loc: storage)
+            enc = torch.load(pretrained_weight_file, 
+                map_location=lambda storage, loc: storage)
             self.embedding_dim = enc['0.encoder.weight'].shape[1]
-            self.hidden_dim = int(enc['0.rnns.0.module.weight_hh_l0_raw'].shape[0]/4)
+            self.hidden_dim = int(
+                enc['0.rnns.0.module.weight_hh_l0_raw'].shape[0]/4)
             self.lstm_layers = 3
             
             new_enc = {}
@@ -126,12 +146,14 @@ class SMaPPLearn:
                 if len(layer_detail) == self.lstm_layers: 
                     new_enc[f'{layer_detail[1]}.{layer_name}'] = v
                 else:
-                    new_enc[f'{layer_detail[1]}.{layer_detail[2]}.{layer_name}'] = v
+                    new_enc[f'{layer_detail[1]}.{layer_detail[2]}.{layer_name}'
+                        ] = v
             
             del new_enc['encoder_with_dropout.embed.weight']
             
             pretrained_idx2word = pickle.load(open(pretrained_itos, 'rb'))
-            pretrained_word2idx = {k: i for i,k in enumerate(pretrained_idx2word)}
+            pretrained_word2idx =\
+                {k: i for i,k in enumerate(pretrained_idx2word)}
             
             new_model_vectorizer = self.vectorizer
             pretrained_encoder_weights = enc['0.encoder.weight']
@@ -140,17 +162,20 @@ class SMaPPLearn:
             row_m = [x.item() for x in row_m]
             
             new_vocab_size = len(new_model_vectorizer.word2idx)
-            new_encoder_weights = torch.tensor([row_m for i in range(new_vocab_size)])
+            new_encoder_weights = torch.tensor(
+                [row_m for i in range(new_vocab_size)])
             
             new_idx2weights = {}
             for word, i in new_model_vectorizer.word2idx.items():
                 if word in pretrained_word2idx:
                     word_idx = pretrained_word2idx[word]
-                    new_encoder_weights[i] = pretrained_encoder_weights[word_idx]
+                    new_encoder_weights[i] =\
+                        pretrained_encoder_weights[word_idx]
 
             new_enc['encoder.weight'] = new_encoder_weights
             new_enc['decoder.weight'] = copy.copy(new_encoder_weights)
-            new_enc['decoder.bias'] = torch.zeros(new_enc['decoder.weight'].shape[0])
+            new_enc['decoder.bias'] =\
+                torch.zeros(new_enc['decoder.weight'].shape[0])
 
             self.lm = RNNLM(device=self.device, vocab_size=new_vocab_size, 
                 embedding_size=self.embedding_dim, hidden_size=self.hidden_dim, 
@@ -177,7 +202,8 @@ class SMaPPLearn:
         self.loss = nn.CrossEntropyLoss()
 
         # Extract pointers to the parameters of the lstms
-        param_list = [{'params': rnn.parameters(), 'lr': 1e-3} for rnn in self.lm.rnns]
+        param_list = [{'params': rnn.parameters(), 'lr': 1e-3} 
+            for rnn in self.lm.rnns]
 
         # If weights are tied between encoder and decoder, we can only optimize 
         # parameters in one of those two layers
@@ -193,8 +219,9 @@ class SMaPPLearn:
 
         self.optimizer = torch.optim.Adam(param_list, lr=0.01)
         print("Beginning LM Fine Tuning")
+        self.freezeTo(3)
         history = training_loop(batch_size=self.batch_size, 
-                                num_epochs=self.num_epochs,
+                                num_epochs=1,
                                 display_freq=self.display_epoch_freq, 
                                 model=self.lm, 
                                 criterion=self.loss,
@@ -206,15 +233,48 @@ class SMaPPLearn:
                                 best_model_path=self.model_file_lm,
                                 history=None)
 
+        self.freezeTo(2)
+        history = training_loop(batch_size=self.batch_size, 
+                                num_epochs=1,
+                                display_freq=self.display_epoch_freq, 
+                                model=self.lm, 
+                                criterion=self.loss,
+                                optim=self.optimizer,
+                                scheduler=None,
+                                device=self.device,
+                                training_set=self.train_dl,
+                                validation_set=self.valid_dl,
+                                best_model_path=self.model_file_lm,
+                                history = history)
+        
+        self.freezeTo(0)
+        history = training_loop(batch_size=self.batch_size, 
+                                num_epochs=self.num_epochs-2,
+                                display_freq=self.display_epoch_freq, 
+                                model=self.lm, 
+                                criterion=self.loss,
+                                optim=self.optimizer,
+                                scheduler=None,
+                                device=self.device,
+                                training_set=self.train_dl,
+                                validation_set=self.valid_dl,
+                                best_model_path=self.model_file_lm,
+	                        history = history)
+	
 if __name__ == '__main__':
     
-    test = SMaPPLearn(data_dir = '/home/vishakh/Stuff/SMaPP/', 
-        train_file = '/home/vishakh/Stuff/SMaPP/unsup.csv', 
-        valid_file = '/home/vishakh/Stuff/SMaPP/valid.csv',  
-        max_vocab_size = 10000, revectorize = False)
+    test = SMaPPLearn(data_dir = '/home/vis/ImdbData/', 
+        train_file = '/home/vis/ImdbData/unsup.csv', 
+        valid_file = '/home/vis/ImdbData/valid.csv',  
+        max_vocab_size = 20000, revectorize = False)
     
+    test.fit_language_model(lm_embedding_dim = 200, lm_hidden_dim = 250, 
+        num_epochs = 10, display_epoch_freq = 1)
+    """ 
     test.fit_language_model(
-        pretrained_weights_file = '/home/vishakh/Stuff/SMaPP/weights-pretrained/fwd_wt103.h5', 
-        pretrained_itos = '/home/vishakh/Stuff/SMaPP/weights-pretrained/itos_wt103.pkl'
-        lm_hidden_dim = 50, lm_embedding_dim=50, 
-        display_epoch_freq = 1, num_epochs = 1)
+        pretrained_weight_file = 
+            '/home/vis/ImdbData/weights_pretrained/fwd_wt103.h5', 
+        pretrained_itos = 
+            '/home/vis/ImdbData/weights_pretrained/fitos_wt103.pkl',
+        display_epoch_freq = 1, num_epochs = 15)
+    """
